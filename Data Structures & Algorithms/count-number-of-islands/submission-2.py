13d0c11c536e368777cad8525378bbddede505c4
s-prshah <113018168+s-prshah@users.noreply.github.com>
2026-04-29 02:57:55 -0700

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # DFS: recursive traversal to explore all connected components
+        # BFS: level by level traversal using a queue
+
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]] # down, up, right, left
+        num_islands = 0
+        def dfs(row, col):
+            if (row < 0 or col < 0 or row >= len(grid) or col >= len(grid[row]) or grid[row][col] == "0"):
+                return
+            
+            grid[row][col] = "0"
+
+            for dr, dc in directions:
+                dfs(row + dr, col + dc)
+            
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == "1":
+                    num_islands += 1
+                    dfs(i, j)
+        
+        return num_islands
+
